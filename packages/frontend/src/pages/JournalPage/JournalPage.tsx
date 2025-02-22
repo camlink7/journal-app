a11d2430074fe0987231fba38f8f474f2f36e02e
Camden <camdenlink7@gmail.com>
2025-02-22 00:47:21 -0500

    Frontend ability to select sample entries

--- packages/frontend/src/pages/JournalPage/JournalPage.tsx
+++ packages/frontend/src/pages/JournalPage/JournalPage.tsx
@@ -1,42 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./JournalPage.css";
 import JournalEntryCard from "./components/JournalEntryCard/JournalEntryCard";
 
 export default function JournalPage(){
 
+    const keys = ["123f", "f3a", "fab"];
+
+    const [selectedEntry, setSelectedEntry] = useState<string | null>(null);
+
+    const handleSelectEntry = ((entryId: string) => {
+        setSelectedEntry(entryId);
+    })
 
     return (
         <>
             <div className="journal-page-grid lg:grid lg:grid-cols-[3fr_10fr] 
                 w-full">
-                <div className="journal-entries-container min-h-screen flex justify-center items-start
-                    bg-base-300 w-full shadow-[4px_0_10px_rgba(0,0,0,0.2)]">
+                <div className="journal-entries-container min-h-screen flex justify-center
+                    bg-base-300 w-full shadow-[4px_0_10px_rgba(0,0,0,0.2)] items-stretch">
                     
-                    <div className="grid border-none w-full">
-                        <div className="bg-base-200 py-1">
-                            <h1 className="text-3xl my-12">Journal Entries</h1>
+                    <div className="grid grid-rows-[1fr_6fr] border-none w-full h-full">
+                        <div className="bg-base-200 py-1 flex items-center justify-center">
+                            <h1 className="text-3xl">Journal Entries</h1>
                         </div>
-                        <div className="h-100 w-full max-h-screen overflow-y-scroll">
-                            <JournalEntryCard/>
-                            <JournalEntryCard/>
-                            <JournalEntryCard/>
-                            <JournalEntryCard/>
-                            <JournalEntryCard/>
-                            <JournalEntryCard/>
-                            <JournalEntryCard/>
-                            <JournalEntryCard/>
-                            <JournalEntryCard/>
-                            <JournalEntryCard/>
-                            <JournalEntryCard/>
-                            <JournalEntryCard/>
-                            <JournalEntryCard/>
-                            <JournalEntryCard/>
+                        <div className="w-full max-h-screen overflow-y-scroll ">
+                            { keys.map((key) => {
+                                    return <JournalEntryCard key={key} entryData={{
+                                        entry_id: key,
+                                        title: "The Worst Day!",
+                                        last_updated_unix: 1740191795,
+                                        tags: ["Unfortunate", "Statistics", "Bad"]
+                                    }
+                                    } onClickHandler={handleSelectEntry} selected={selectedEntry === key}/>
+                                })
+                            }
                         </div>
                     </div>
                 </div>
-                <div className="active-journal-container lg:block hidden">
+                <div className="active-journal-container lg:block items-stretch hidden">
                     <div className="active-journal-container-grid grid lg:grid-rows-[1fr_6fr]
-                        grid-rows-1fr">
+                        grid-rows-1">
                         <div className="flex justify-center items-center bg-base-200">
                             <p>Active Journal Header</p>
                         </div>
--- packages/frontend/src/pages/JournalPage/components/JournalEntryCard/JournalEntryCard.tsx
+++ packages/frontend/src/pages/JournalPage/components/JournalEntryCard/JournalEntryCard.tsx
@@ -1,21 +1,23 @@
 import React from "react";
 import "./JournalEntryCard.css";
 
-export default function JournalEntryCard(){
+interface JournalEntryCardProps {
+    entryData: {entry_id: string, title: string, last_updated_unix: string | number, tags: Array<string>},
+    onClickHandler: (entryId: string) => void,
+    selected?: boolean
+}
 
-    const data = {
-        entry_id: "3fae3-qera3a-fj3afhz",
-        title: "The Worst Day!",
-        last_updated_unix: 1740191795,
-        tags: ["Unfortunate", "Statistics", "Bad"]
-    }
+const JournalEntryCard: React.FC<JournalEntryCardProps> = ({entryData, onClickHandler, selected = false}) => {
 
-    return (<div className="journal-entry-card w-full p-5 text-left max-h-fit 
-        border-b-2 border-gray-600 hover:bg-neutral-800">
-        <h1 className="text-xl font-bold my-2">{data.title}</h1>
-        <p>{new Date(data.last_updated_unix * 1000).toLocaleString()}</p>
+    return (<div className={`journal-entry-card w-full p-5 text-left max-h-fit 
+        border-b-2 border-gray-600 ${(selected !== null && selected === false) && "hover:bg-blue-400 hover:bg-opacity-25"}
+         ${selected && "border-blue-300 border-t-2 bg-blue-500 bg-opacity-40"}`}
+            onClick={() => onClickHandler(entryData.entry_id)}>
+
+        <h1 className="text-xl font-bold my-2">{entryData.title}</h1>
+        <p>{new Date(Number(entryData.last_updated_unix) * 1000).toLocaleString()}</p>
         <div className="flex my-3">
-            {data.tags.map((tag) => {
+            {entryData.tags.map((tag) => {
                 return (<div className="entry-tag bg-blue-800 mr-2 px-2 rounded-full">
                         {tag}
                     </div>)
@@ -23,3 +25,5 @@ export default function JournalEntryCard(){
         </div>
     </div>);
 }
+
+export default JournalEntryCard;
